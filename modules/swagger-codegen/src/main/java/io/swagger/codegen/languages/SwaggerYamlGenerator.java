e95e5ceee1d8be1a80925c2460f8b8bee8f81d27
Kevin Brown <kjbrown@stanford.edu>
2018-07-25 14:26:47 -0700

    Sort output spec for YAML codegen

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SwaggerYamlGenerator.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SwaggerYamlGenerator.java
@@ -1,6 +1,7 @@
 package io.swagger.codegen.languages;
 
 import io.swagger.codegen.*;
+import io.swagger.models.Tag;
 import io.swagger.models.Swagger;
 import io.swagger.util.Yaml;
 import org.apache.commons.io.FileUtils;
@@ -8,6 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SwaggerYamlGenerator extends DefaultCodegen implements CodegenConfig {
     public static final String OUTPUT_NAME = "outputFile";
@@ -23,7 +28,7 @@ public class SwaggerYamlGenerator extends DefaultCodegen implements CodegenConfi
 
         cliOptions.add(new CliOption(OUTPUT_NAME, "output filename"));
 
-        supportingFiles.add(new SupportingFile("README.md", "", "README.md"));
+        // supportingFiles.add(new SupportingFile("README.md", "", "README.md"));
     }
 
     @Override
@@ -53,6 +58,7 @@ public class SwaggerYamlGenerator extends DefaultCodegen implements CodegenConfi
     @Override
     public void processSwagger(Swagger swagger) {
         try {
+            cleanupSwagger(swagger);
             String swaggerString = Yaml.mapper().writeValueAsString(swagger);
             String outputFile = outputFolder + File.separator + this.outputFile;
             FileUtils.writeStringToFile(new File(outputFile), swaggerString);
@@ -72,6 +78,26 @@ public class SwaggerYamlGenerator extends DefaultCodegen implements CodegenConfi
     public String escapeUnsafeCharacters(String input) {
         // just return the original string
         return input;
-    }   
+    }
+
+    private void cleanupSwagger(Swagger swagger) {
+        // sort paths, models, and tags so diffs are consistent
+        swagger.setPaths(new TreeMap<>(swagger.getPaths()));
+        swagger.setDefinitions(new TreeMap<>(swagger.getDefinitions()));
+
+        Comparator<Tag> comp = new Comparator<Tag>() {
+            public int compare(Tag a, Tag b) {
+                return a.getName().compareTo(b.getName());
+            }
+        };
+        List<Tag> tags = swagger.getTags();
+        tags.sort(comp);
+        swagger.setTags(tags);
+
+        // remove auto-generated examples
+        for (io.swagger.models.Model def : swagger.getDefinitions().values()) {
+            def.setExample(null);
+        }
+    }
 
 }
